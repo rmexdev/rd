83995acdd1b01f15c5f5b396fd7f2a44ad28ac2f
rmexdev <rmexdev@proton.me>
2026-09-15 23:32:36 +0530

    snp: Init code editor in note

--- clients/stickier-notes/src/note/note.tsx
+++ clients/stickier-notes/src/note/note.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
+import ReactCodeMirror from '@uiw/react-codemirror';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 
 export function Note() {
     const [title, setTitle] = useState<string>('Placeholder...');
+    const [content, setContent] = useState<string>('');
 
     useEffect(() => {
         const noteWebView = getCurrentWebviewWindow();
@@ -18,7 +21,21 @@ export function Note() {
         };
     }, []);
 
-    return <div>{title}</div>;
+    return (
+        <>
+            <div>{title}</div>
+            <ReactCodeMirror
+                value={content}
+                onChange={setContent}
+                autoFocus
+                extensions={[
+                    markdown({
+                        base: markdownLanguage,
+                    }),
+                ]}
+            />
+        </>
+    );
 }
 
 export default Note;
